4c1178ab7a7aeabc6d489d6cf0be76934f7bda0d
idan131224 <idan131224@gmail.com>
2022-11-19 15:41:57 +0200

    final commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+)
